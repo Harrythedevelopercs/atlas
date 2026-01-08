8da6a8042b3f305defa9d03611355998482e8429
Harrythedevelopercs <harrykennedy.cs@gmail.com>
2026-01-08 19:10:53 +0500

    new page conditions

---
+++ Terms-and-conditions.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include 'include/style.php'; ?>
+    <title>Terms And Conditions | Atlas Publishing Group</title>
+    <meta name="description"
+        content="Atlas Publishing Group is your trusted partner in book publishing and marketing—offering editing, design, global distribution, and promotional support to help authors succeed.">
+</head>
+
+<body>
+    <?php include 'include/header.php'; ?>
+    <div class="banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <h2>Terms And Conditionsy</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="term_content py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <p>
+                        When you complete an order placement with us i.e. you make the payment, then you are considered
+                        to have read, understood and agreed to terms and conditions listed on this page. These terms are
+                        a legal contract between the client and Novella Publishers. By agreeing to order and use the
+                        services from Novella Publishers you are deemed to be bound by the terms which as stated as
+                        follows.
+                    </p>
+                    <br><br>
+                    <h3>We Deliver Original Content Only</h3>
+                    <p>
+                        We guarantee you plagiarism free content. To ensure that content is original we provide you with
+                        a Copyscape report. (Note: Any text contained in images, videos or other media, which search
+                        engines can't read will not be scanned by Copyscape.) You may go for more comprehensive and
+                        deeper plagiarism checks than Copyscape by yourself because we do not cover such services in our
+                        standard prices. You can be sure of the work you get from Novella Publishers in terms of its
+                        originality.
+                    </p>
+                    <br><br>
+                    <h3>We Deliver Content With Complete Ownership</h3>
+                    <p>
+                        With every complete order, you get complete ownership of the content the team writes for you.
+                        You will have to publish the content under your name and/or will be able to utilize the content
+                        the way you desire.
+                    </p>
+                    <br><br>
+                    <h3>How do we protect your information?</h3>
+                    <p>
+                        The moment you place an order or enter, submit any of your personal information we implement
+                        security measures at all levels to maintain the safety of your personal information. By using
+                        SSL, we ensure the security and confidentiality of every information you share with us. After
+                        the completion of a transaction, we do not store any of your private information (credit cards,
+                        social security numbers, financials, etc.) in our database.
+                    </p>
+                    <br><br>
+                    <h3>Why do we use cookies?</h3>
+                    <p>
+                        We use cookies to keep track of advertisements and compile aggregate data about site traffic and
+                        site interactions. It enables us to offer better site experiences and tools to the users. If
+                        required, we may seek services from a third-party to assist us in understanding the behavior of
+                        visitors. However, third-party service providers are not allowed to use the collected
+                        information except for the sole purpose of customers’ analysis.
+                    </p>
+                    <br><br>
+                    <h3>Our Policy For Using Images</h3>
+                    <p>
+                        We believe in abiding by the rules and maintaining our credibility for the long term both with
+                        the clients and in the market. We do not own the copyright of images, hence we do not use
+                        copyright images by ourselves. We will simply provide you a link of recommended images so you
+                        can copy and paste them by yourself. Besides, these links can help you have an idea of the
+                        images that may suit the content. Otherwise, you may use royalty free images without any worry.
+                        However, for copyright images, you may be required to pay the owner(s).
+                    </p>
+                    <br><br>
+                    <h3>Limitation Of Liability</h3>
+                    <p>
+                        The users are advised to take into consideration this clause carefully. We, Novella Publishers,
+                        with this declare that under any circumstances we shall not be liable for any direct or indirect
+                        loss claimed by our customer(s) to have resulted from the use of our website or
+                        services/products sold through our website. This disclaimer also applies to any viruses that may
+                        allegedly be obtained by using our website/products and may have caused any type of material or
+                        reputational loss. We shall, in no event, be liable for any liability or loss that may result
+                        from the inability or ability of the user of our website/products.
+                    </p>
+                    <br><br>
+                    <h3>We Update Our Policies</h3>
+                    <p>
+                        This privacy policy is subject to change; hence, you are required to visit our policy page from
+                        time to time to see if there is an update. The policy changes are not subject to any prior
+                        notice and time frame.
+                    </p>
+                    <br><br>
+                    <h3>Refund Policy</h3>
+                    <p>
+                        This privacy policy is subject to change; hence, you are required to visit our policy page from
+                        time to time to see if there is an update. The policy changes are not subject to any prior
+                        notice and time frame.
+                    </p>
+                    <br><br>
+                    <h3>Full Refund</h3>
+                    <p>
+                        With Novella Publishers you may avail the option of full refund. We value our clients and try
+                        all possible ways to assist them. If the work on your project has started and you somehow need
+                        to stop or claim refund you can discuss the status of your work with our support team member to
+                        assist you further. The support and assistance will help you provide current work status and the
+                        option you may have in the given situation.
+                    </p>
+                    <br><br>
+                    <h3>Change Of Mind</h3>
+                    <p>
+                        If (for any reason) you change your mind and decide against continuing your project with us
+                        after placing your order, you can ask for a refund within the first hour of placing your order.
+                        A 60% processing fee will be charged in other cases.
+                    </p>
+                    <br><br>
+                    <h3>Incompetent Delivery</h3>
+                    <p>
+                        Once the work is delivered, customers are only entitled to claim a refund once they have
+                        exhausted all the options detailed as under;
+                        If it does not comply with project requirements (as requested/documented by the customer). We
+                        are committed to provide our customers with 100% satisfaction and offer unlimited revisions to
+                        ensure that the delivery is up to the mark. We assign, re-assign and re-write your work to
+                        ensure complete satisfaction.
+                        <br><br>
+                        Reserve FREE Pages for future, but of same value and you can avail them at any time.
+                        If we’re still not able to deliver what you asked for, refund will be processed with a mutual
+                        agreement on a set percentage (but only in the cases where the delivery is completely off the
+                        mark)
+                    </p>
+                    <br><br>
+                    <h3>Refund Time Frame</h3>
+                    <p>
+                        Refunds must be claimed within 120 days of delivery. Refunds claimed after the set time frame
+                        will not be entertained.
+                        All the customers must note the deadline for claiming refund at the time of placing order.
+                    </p>
+                    <br><br>
+                    <h3>Cases Where Refund Will Not Be Issued</h3>
+                    <p>
+                        In case of late delivery due to some minor technical errors, such as grammatical, typing, word
+                        count, missing references etc., refunds will be processed with mutual agreement and the company
+                        will only settle with partial refund or discounts reserved for future purchases.
+                        The company will not be responsible for any delay from the client’s end.
+                        No refund will be issued on the basis of low writing.
+                        For book marketing/publishing projects, no refund will be issued if the client is unable to
+                        provide access to either their book stores (Amazon, Barnes & Noble etc.) or their social media
+                        channels which helps our team to maximize reach, engagement and sales.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'include/footer.php'; ?>
